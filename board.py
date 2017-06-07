277d8840fea0428e9380d6ed2fb5bce79342f1ed
John Chang <johnchang9094@gmail.com>
2017-06-07 22:41:17 +0100

    Implemented Tic Tac Toe Logic, Adding 'o' or 'x'

--- board.py
+++ board.py
@@ -1,3 +1,4 @@
+import random
 
 # Game board
 board = [0, 1, 2,
@@ -5,8 +6,32 @@ board = [0, 1, 2,
         6, 7, 8]
 
 def show():
-    print (board[0], '|',board[1], '|', board[2])
-    print ('----------')
-    print (board[3], '|',board[4], '|', board[5])
-    print ('----------')
-    print (board[6], '|',board[7], '|', board[8])
+    print board[0], '|',board[1], '|', board[2]
+    print '----------'
+    print board[3], '|',board[4], '|', board[5]
+    print '----------'
+    print board[6], '|',board[7], '|', board[8]
+
+while True:
+
+    input= raw_input("Select a Spot: ")
+    input = int(input)
+
+    if board[input] != 'x' and board[input] != 'o':
+        board[input] = 'x'
+
+        finding = True
+
+        while finding:
+            random.seed() # Gives a random number, random generator
+            opponent = random.randint(0, 8)
+
+            if board[opponent] != 'o' and board[opponent] != 'x':
+                board[opponent] = 'o'
+                finding = False
+
+    else:
+        print ("This spot is taken!")
+
+    show()
+
