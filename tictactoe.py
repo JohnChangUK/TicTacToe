a6c699c30bd971afe97cf3a54ee1302bd90c4f58
John Chang <johnchang9094@gmail.com>
2017-06-08 22:38:32 +0100

    All 8 Winning Options

--- tictactoe.py
+++ tictactoe.py
@@ -32,7 +32,15 @@ while True:
         time.sleep(1)
 
     # Check for X win
-    if (board[1] == "X" and board[2] == "X" and board[3] == "X"):
+    if (board[1] == "X" and board[2] == "X" and board[3] == "X") or \
+        (board[4] == "X" and board[5] == "X" and board[6] == "X") or \
+        (board[7] == "X" and board[8] == "X" and board[9] == "X") or \
+        (board[1] == "X" and board[4] == "X" and board[7] == "X") or \
+        (board[2] == "X" and board[5] == "X" and board[8] == "X") or \
+        (board[3] == "X" and board[6] == "X" and board[9] == "X") or \
+        (board[1] == "X" and board[5] == "X" and board[9] == "X") or \
+        (board[3] == "X" and board[5] == "X" and board[7] == "X"):
+        os.system("Clear")
+        print_board()
         print "X Wins! Congrats!"
         break
-        
