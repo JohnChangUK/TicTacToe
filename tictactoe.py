f2ab95442a64902c5f5e6b2a95afde55f2939189
John Chang <johnchang9094@gmail.com>
2017-06-08 22:23:12 +0100

    Setting up Board

---
+++ tictactoe.py
@@ -0,0 +1,21 @@
+import os
+import time
+import random
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ",]
+
+def print_board():
+    print "   |   |   "
+    print " "+board[1]+" | |"+board[2]+" | "+board[3]+" "
+    print "   |   |   "
+    print "---|---|---"
+    print "   |   |   "
+    print " "+board[4]+" | |"+board[5]+" | "+board[6]+" "
+    print "   |   |   "
+    print "---|---|---"
+    print "   |   |   "
+    print " "+board[7]+" | |"+board[8]+" | "+board[9]+" "
+    print "   |   |   "
+
+while True:
+    
