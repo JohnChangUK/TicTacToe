39779125b48209488ee42309026aac0215408b9b
John Chang <johnchang9094@gmail.com>
2017-06-09 14:17:19 +0100

    Tie Logic Implemented

--- tictactoe.py
+++ tictactoe.py
@@ -17,32 +17,24 @@ def print_board():
     print " "+board[7]+" | "+board[8]+" | "+board[9]+" "
     print "   |   |   "
 
-def is_x_winner(board):
-    if (board[1] == "X" and board[2] == "X" and board[3] == "X") or \
-        (board[4] == "X" and board[5] == "X" and board[6] == "X") or \
-        (board[7] == "X" and board[8] == "X" and board[9] == "X") or \
-        (board[1] == "X" and board[4] == "X" and board[7] == "X") or \
-        (board[2] == "X" and board[5] == "X" and board[8] == "X") or \
-        (board[3] == "X" and board[6] == "X" and board[9] == "X") or \
-        (board[1] == "X" and board[5] == "X" and board[9] == "X") or \
-        (board[3] == "X" and board[5] == "X" and board[7] == "X"):
+def is_winner(board, player):
+    if (board[1] == player and board[2] == player and board[3] == player) or \
+        (board[4] == player and board[5] == player and board[6] == player) or \
+        (board[7] == player and board[8] == player and board[9] == player) or \
+        (board[1] == player and board[4] == player and board[7] == player) or \
+        (board[2] == player and board[5] == player and board[8] == player) or \
+        (board[3] == player and board[6] == player and board[9] == player) or \
+        (board[1] == player and board[5] == player and board[9] == player) or \
+        (board[3] == player and board[5] == player and board[7] == player):
         return True
     else:
         return False
 
-def is_o_winner(board):
-    if (board[1] == "O" and board[2] == "O" and board[3] == "O") or \
-        (board[4] == "O" and board[5] == "O" and board[6] == "O") or \
-        (board[7] == "O" and board[8] == "O" and board[9] == "O") or \
-        (board[1] == "O" and board[4] == "O" and board[7] == "O") or \
-        (board[2] == "O" and board[5] == "O" and board[8] == "O") or \
-        (board[3] == "O" and board[6] == "O" and board[9] == "O") or \
-        (board[1] == "O" and board[5] == "O" and board[9] == "O") or \
-        (board[3] == "O" and board[5] == "O" and board[7] == "O"):
-        return True
-    else:
+def is_board_full(board):
+    if " " in board:
         return False
-
+    else:
+        return True
 
 while True:
     os.system("Clear")
@@ -60,7 +52,7 @@ while True:
         time.sleep(1)
 
     # Check for X win
-    if is_x_winner(board):
+    if is_winner(board, "X"):
         os.system("Clear")
         print_board()
         print "X Wins! Congrats!"
@@ -70,14 +62,11 @@ while True:
     print_board()
 
     # Check for a Tie (Board full with no Winning Matches)
-    isFull = True
-    if " " in board:
-        isFull = False
-
     # If board is full, display Message
-    if isFull:
-        print "It's a Tie!"
+    if is_board_full(board):
+        print "Tie!"
         break
+   
 
     # Get player O input
     choice = raw_input("Please choose an empty space for O. ")
@@ -91,19 +80,16 @@ while True:
         time.sleep(1)
 
     # Check for O win
-    if is_o_winner(board):
+    if is_winner(board, "O"):
         os.system("Clear")
         print_board()
         print "O Wins! Congrats!"
         break
 
     # Check for a Tie (Board full with no Winning Matches)
-    isFull = True
-    if " " in board:
-        isFull = False
-
     # If board is full, display Message
-    if isFull:
-        print "It's a Tie!"
+    if is_board_full(board):
+        print "Tie!"
         break
+    
 
