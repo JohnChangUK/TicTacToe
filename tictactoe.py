3f437d6b32ab404a40b043fb0f2340cf4483c9ab
John Chang <johnchang9094@gmail.com>
2017-06-09 13:46:23 +0100

    Refactored X and O Winning logic into functions

--- tictactoe.py
+++ tictactoe.py
@@ -17,6 +17,33 @@ def print_board():
     print " "+board[7]+" | "+board[8]+" | "+board[9]+" "
     print "   |   |   "
 
+def is_x_winner(board):
+    if (board[1] == "X" and board[2] == "X" and board[3] == "X") or \
+        (board[4] == "X" and board[5] == "X" and board[6] == "X") or \
+        (board[7] == "X" and board[8] == "X" and board[9] == "X") or \
+        (board[1] == "X" and board[4] == "X" and board[7] == "X") or \
+        (board[2] == "X" and board[5] == "X" and board[8] == "X") or \
+        (board[3] == "X" and board[6] == "X" and board[9] == "X") or \
+        (board[1] == "X" and board[5] == "X" and board[9] == "X") or \
+        (board[3] == "X" and board[5] == "X" and board[7] == "X"):
+        return True
+    else:
+        return False
+
+def is_o_winner(board):
+    if (board[1] == "O" and board[2] == "O" and board[3] == "O") or \
+        (board[4] == "O" and board[5] == "O" and board[6] == "O") or \
+        (board[7] == "O" and board[8] == "O" and board[9] == "O") or \
+        (board[1] == "O" and board[4] == "O" and board[7] == "O") or \
+        (board[2] == "O" and board[5] == "O" and board[8] == "O") or \
+        (board[3] == "O" and board[6] == "O" and board[9] == "O") or \
+        (board[1] == "O" and board[5] == "O" and board[9] == "O") or \
+        (board[3] == "O" and board[5] == "O" and board[7] == "O"):
+        return True
+    else:
+        return False
+
+
 while True:
     os.system("Clear")
     print_board()
@@ -33,14 +60,7 @@ while True:
         time.sleep(1)
 
     # Check for X win
-    if (board[1] == "X" and board[2] == "X" and board[3] == "X") or \
-        (board[4] == "X" and board[5] == "X" and board[6] == "X") or \
-        (board[7] == "X" and board[8] == "X" and board[9] == "X") or \
-        (board[1] == "X" and board[4] == "X" and board[7] == "X") or \
-        (board[2] == "X" and board[5] == "X" and board[8] == "X") or \
-        (board[3] == "X" and board[6] == "X" and board[9] == "X") or \
-        (board[1] == "X" and board[5] == "X" and board[9] == "X") or \
-        (board[3] == "X" and board[5] == "X" and board[7] == "X"):
+    if is_x_winner(board):
         os.system("Clear")
         print_board()
         print "X Wins! Congrats!"
@@ -71,14 +91,7 @@ while True:
         time.sleep(1)
 
     # Check for O win
-    if (board[1] == "O" and board[2] == "O" and board[3] == "O") or \
-        (board[4] == "O" and board[5] == "O" and board[6] == "O") or \
-        (board[7] == "O" and board[8] == "O" and board[9] == "O") or \
-        (board[1] == "O" and board[4] == "O" and board[7] == "O") or \
-        (board[2] == "O" and board[5] == "O" and board[8] == "O") or \
-        (board[3] == "O" and board[6] == "O" and board[9] == "O") or \
-        (board[1] == "O" and board[5] == "O" and board[9] == "O") or \
-        (board[3] == "O" and board[5] == "O" and board[7] == "O"):
+    if is_o_winner(board):
         os.system("Clear")
         print_board()
         print "O Wins! Congrats!"
