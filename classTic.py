c01679aad418581cbeae8b45ed39897874e76100
John Chang <johnchang9094@gmail.com>
2017-06-11 12:22:40 +0100

    Board Update Cell method added

--- classTic.py
+++ classTic.py
@@ -13,6 +13,28 @@ class Board():
         print ("-----------")
         print (" %s | %s | %s " %(self.cells[7], self.cells[8], self.cells[9]))
 
+    def update_cell(self, cell_no, player):
+        self.cells[cell_no] = player
 
 board = Board()
 board.display()
+
+def print_header():
+    print("Play Tic Tac Toe! \n")
+
+def refresh_screen():
+    os.system("clear")
+
+    print_header()
+
+    board.display()
+
+# while True acts an as Infinite loop
+while True:
+    refresh_screen()
+
+    # Get X input
+    x_choice = int(raw_input("\nX) Please choose 1 - 9. > "))
+
+    # Updating Board
+    board.update_cell(x_choice, "X")
